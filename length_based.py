ab6cdb58b0b1cbf0f57c1dcf8e78c4fde485a251
uneiz1103 <uneizshaikh1103@gmail.com>
2025-08-02 12:04:29 +0530

    feat: implement character-based text splitting using fixed chunk length

--- length_based.py
+++ length_based.py
@@ -1,1 +1,19 @@
- 
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader(
+    file_path='dl-curriculum.pdf'
+)
+
+docs =  loader.load()
+
+splitter = CharacterTextSplitter(
+    chunk_size = 200,
+    chunk_overlap = 0,
+    separator=''
+)
+
+result = splitter.split_documents(docs)
+
+print(result[1].page_content)
+
